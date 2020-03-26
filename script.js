e06b950ab41ccd41343ccba2099f4950ef534f37
nadilaoktaviani2 <nadilaoktaviani2@gmail.com>
2020-03-26 23:43:21 +0700

    lanjut bem

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+    const EFFECT = document.querySelector("welcome");
+    window.addEventListener('scroll',scrollEffect);
+
+    function scrollEffect (){
+        if(window.scrollY >= 500){
+            EFFECT.style.opacity = '1';
+            EFFECT.style.transform = 'translateX(0px)';
+            EFFECT.style.transition = '1s ease-in-out';
+            
+            
+        }
+        else{
+            EFFECT.style.opacity = '0';
+            EFFECT.style.transform = 'translateX(-50px)';
+            
+        }
+    }
+}
